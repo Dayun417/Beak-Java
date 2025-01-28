bc9df715fd32c36a1866a444692ff75a35f909bf
Dayun417 <leedayun03417@gmail.com>
2025-01-28 21:05:59 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        char char1 = sc.next().charAt(0);
+        System.out.println((int)char1);
+
+        sc.close();
+    }
+}
