a8cc35543924a005f35fb293f1f3547a043dbec4
Lee Dayun <leedayun03417@gmail.com>
2026-02-27 18:52:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -7,22 +7,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int arr[] = new int[26];
-        Arrays.fill(arr, -1);
-
         String s = br.readLine();
         
-        for(int i=0;i<s.length();i++) {
-            int n = s.charAt(i) - 'a';    
-
-            if(arr[n] == -1) {
-                arr[n] = i;
-            }
-        }
-
         for(int i=0;i<26;i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(s.indexOf(i+'a') + " ");
         }
-
     }
 }
